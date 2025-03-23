cffc8b2c2f0edc6151e28dacfea5d11abecec627
rodriguezvla <rodriguezveralautaro@fpllefia.com>
2025-03-23 19:57:00 +0100

    bdnoticias

---
+++ bdNoticies.js
@@ -0,0 +1,54 @@
+
+const bd = 
+[
+    {
+    titulo: "Sed nec lorem scelerisque, viverra ex ut, interdum massa.",
+    descripcion: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas eu ipsum volutpat, maximus lorem eu, ultricies lorem. Sed blandit magna, nullam feugiat quam quis venenatis blandit.",
+    imagen: "./imagenes/article 1.png",
+    },
+
+    {
+    titulo: "Morbi ligula massa, posuere in finibus ut, varius ac ligula.",
+    descripcion: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas eu ipsum volutpat, maximus lorem eu, ultricies lorem. Sed blandit magna, nullam feugiat quam quis venenatis blandit.",
+    imagen: "./imagenes/article 2.png",
+    },
+
+    {
+        titulo: "Sed nec lorem scelerisque, viverra ex ut, interdum massa.",
+        descripcion: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas eu ipsum volutpat, maximus lorem eu, ultricies lorem. Sed blandit magna, nullam feugiat quam quis venenatis blandit.",
+        imagen: "./imagenes/article 3.png",
+    },
+
+    {
+        titulo: "Morbi ligula massa, posuere in finibus ut, varius ac ligula.",
+        descripcion: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas eu ipsum volutpat, maximus lorem eu, ultricies lorem. Sed blandit magna, nullam feugiat quam quis venenatis blandit.",
+        imagen: "./imagenes/article 1.png",
+    },
+
+    {
+        titulo: "Sed nec lorem scelerisque, viverra ex ut, interdum massa.",
+        descripcion: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas eu ipsum volutpat, maximus lorem eu, ultricies lorem. Sed blandit magna, nullam feugiat quam quis venenatis blandit.",
+        imagen: "./imagenes/article 2.png",
+    },
+]
+
+function mostrarNoticias() {
+    let noticias = "";
+    for (let i = 0; i < bd.length; i++) {
+        noticias += 
+        `<div class="noticia">
+        <div class="noticia-imagen">
+            <img src="${bd[i].imagen}" alt="noticia">
+        </div>
+        <div class="noticia-contenido">
+            <h2>${bd[i].titulo}</h2>
+            <p>${bd[i].descripcion}</p>
+            </div>
+        </div>`;
+    }
+    document.getElementById("news").innerHTML = noticias;
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    mostrarNoticias();
+});
---
+++ enlaces.js
@@ -0,0 +1,4 @@
+const boton = document.querySelector(".contenedor-articulos .boton-explorar");
+
+boton.addEventListener("click",function(e){
+e.preventDefault(); window.location.href = "news.html";})
